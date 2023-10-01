8a6da17c8bf399b13b20d656dcd7c90210eb812a
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-02 00:46:38 +0530

    Add image slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero"
+import ImageSlider from "@/components/ImageSlider"
 import Offers from "@/components/Offers"
 export default function Home() {
   return (
     <main className="">
       <Hero/>
       <Offers/>
+      <ImageSlider/>
     </main>
   )
 }
