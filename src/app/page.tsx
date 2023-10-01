f259ee17ff71e528143dfffe30217de8e38d6ada
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-02 00:27:17 +0530

    Add Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image'
-
+import Hero from "@/components/Hero"
 export default function Home() {
   return (
-    <main className="text-red-400">
-      hello world
+    <main className="">
+      <Hero/>
     </main>
   )
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Hero: React.FC = () => {
+  return (
+    <div className='w-full h-screen '>
+      <Image
+        src="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg"
+        alt=""
+        fill
+        objectFit='cover'
+        className='top-0 left-0 w-full h-full'
+        />
+        <div className='bg-black/30 absolute top-0 left-0 w-full h-screen' />
+        <div className='absolute top-0 w-full h-full flex flex-col justify-center text-white'>
+        <div className='md:left-[10%] max-w-[1100px] m-auto absolute p-4'>
+          <p>All Inclusive</p>
+          <h1 className='font-bold text-5xl md:text-7xl drop-shadow-2xl'>
+            Private Beaches & Getaways
+          </h1>
+          <p className='max-w-[600px] drop-shadow-2xl py-2 text-xl'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ut quis
+            est, id consequuntur sequi ipsum vitae sit non exercitationem.
+          </p>
+          <button className='bg-white text-black '>Reserve Now</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
