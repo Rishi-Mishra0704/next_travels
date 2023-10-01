7a25c9e6f783e3ccb0121709a77e2341a005f6e8
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-02 00:49:50 +0530

    Add footer and plans

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero"
 import ImageSlider from "@/components/ImageSlider"
 import Offers from "@/components/Offers"
+import Plan from "@/components/Plan"
 export default function Home() {
   return (
     <main className="">
       <Hero/>
       <Offers/>
+      <Plan/>
       <ImageSlider/>
     </main>
   )
