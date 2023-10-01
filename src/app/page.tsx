a47d24fa295ddbbec25badb387aadc57dc559523
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-02 01:03:13 +0530

    Add rooms section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Hero from "@/components/Hero"
 import ImageSlider from "@/components/ImageSlider"
 import Offers from "@/components/Offers"
 import Plan from "@/components/Plan"
+import Rooms from "@/components/Rooms"
 export default function Home() {
   return (
     <main className="">
       <Hero/>
       <Offers/>
       <Plan/>
+      <Rooms/>
       <ImageSlider/>
     </main>
   )
